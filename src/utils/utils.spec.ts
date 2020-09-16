c74e4e692db62aae5b5b54b202faa4e9b86512a6
Georgios Kampitakis <50364739+gkampitakis@users.noreply.github.com>
2020-09-17 00:14:50 +0300

    Republish under different name
    * chore: start refactoring for republishing
    
    * feat: add support for allSettled polyfill

--- src/utils/utils.spec.ts
+++ src/utils/utils.spec.ts
@@ -1,87 +1,87 @@
 import { chunkArray, isArray, isObject } from './';
 
 describe('Utils', () => {
-	describe('isArray', () => {
-		it('Should return true', () => {
-			expect(isArray([])).toBe(true);
-		});
-		it('Should return true', () => {
-			expect(isArray([1, 2, 3, 4, 5, 6])).toBe(true);
-		});
-		it('Should return false', () => {
-			expect(isArray({})).toBe(false);
-		});
-		it('Should return false', () => {
-			expect(isArray(() => {})).toBe(false);
-		});
-		it('Should return false', () => {
-			expect(isArray(10)).toBe(false);
-		});
-		it('Should return false', () => {
-			//@ts-ignore
-			expect(isArray()).toBe(false);
-		});
-	});
-	describe('isObject', () => {
-		it('Should return true', () => {
-			expect(isObject({})).toBe(true);
-		});
-		it('Should return true', () => {
-			expect(
-				isObject({
-					test: 'test',
-					mock: 'mockObject'
-				})
-			).toBe(true);
-		});
-		it('Should return false', () => {
-			expect(isObject([])).toBe(false);
-		});
-		it('Should return false', () => {
-			expect(isObject(() => {})).toBe(false);
-		});
-		it('Should return false', () => {
-			expect(isObject(10)).toBe(false);
-		});
-		it('Should return false', () => {
-			// @ts-ignore
-			expect(isObject()).toBe(false);
-		});
-	});
-	describe('chunkArray', () => {
-		it('Should throw error if no object provided', () => {
-			try {
-				//@ts-ignore
-				chunkArray({}, 2);
-			} catch (error) {
-				expect(error.message).toBe('Provide an array');
-			}
-		});
+  describe('isArray', () => {
+    it('Should return true', () => {
+      expect(isArray([])).toBe(true);
+    });
+    it('Should return true', () => {
+      expect(isArray([1, 2, 3, 4, 5, 6])).toBe(true);
+    });
+    it('Should return false', () => {
+      expect(isArray({})).toBe(false);
+    });
+    it('Should return false', () => {
+      expect(isArray(() => { })).toBe(false);
+    });
+    it('Should return false', () => {
+      expect(isArray(10)).toBe(false);
+    });
+    it('Should return false', () => {
+      //@ts-ignore
+      expect(isArray()).toBe(false);
+    });
+  });
+  describe('isObject', () => {
+    it('Should return true', () => {
+      expect(isObject({})).toBe(true);
+    });
+    it('Should return true', () => {
+      expect(
+        isObject({
+          test: 'test',
+          mock: 'mockObject'
+        })
+      ).toBe(true);
+    });
+    it('Should return false', () => {
+      expect(isObject([])).toBe(false);
+    });
+    it('Should return false', () => {
+      expect(isObject(() => { })).toBe(false);
+    });
+    it('Should return false', () => {
+      expect(isObject(10)).toBe(false);
+    });
+    it('Should return false', () => {
+      // @ts-ignore
+      expect(isObject()).toBe(false);
+    });
+  });
+  describe('chunkArray', () => {
+    it('Should throw error if no object provided', () => {
+      try {
+        //@ts-ignore
+        chunkArray({}, 2);
+      } catch (error) {
+        expect(error.message).toBe('Provide an array');
+      }
+    });
 
-		it('Should return an array containing 3 chunks', () => {
-			const array = [1, 2, 3];
-			expect(chunkArray(array, 1).length).toBe(3);
-		});
-		it('Should return an array containing 1 chunk', () => {
-			const array = [1, 2, 3];
-			expect(chunkArray(array, 10).length).toBe(1);
-		});
-		it('Should return an array containing 1 chunk', () => {
-			const array = [1, 2, 3];
-			expect(chunkArray(array, 0).length).toBe(1);
-			expect(chunkArray(array).length).toBe(1);
-		});
+    it('Should return an array containing 3 chunks', () => {
+      const array = [1, 2, 3];
+      expect(chunkArray(array, 1).length).toBe(3);
+    });
+    it('Should return an array containing 1 chunk', () => {
+      const array = [1, 2, 3];
+      expect(chunkArray(array, 10).length).toBe(1);
+    });
+    it('Should return an array containing 1 chunk', () => {
+      const array = [1, 2, 3];
+      expect(chunkArray(array, 0).length).toBe(1);
+      expect(chunkArray(array).length).toBe(1);
+    });
 
-		it('Should return an an array containing an empty array', () => {
-			const array = [];
+    it('Should return an an array containing an empty array', () => {
+      const array = [];
 
-			const res1 = chunkArray(array, 1),
-				res2 = chunkArray(array, 0);
+      const res1 = chunkArray(array, 1),
+        res2 = chunkArray(array, 0);
 
-			expect(res1.length).toBe(1);
-			expect(res1[0].length).toBe(0);
-			expect(res2.length).toBe(1);
-			expect(res2[0].length).toBe(0);
-		});
-	});
+      expect(res1.length).toBe(1);
+      expect(res1[0].length).toBe(0);
+      expect(res2.length).toBe(1);
+      expect(res2[0].length).toBe(0);
+    });
+  });
 });
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,22 +1,22 @@
 function isArray(a): boolean {
-	return !!a && a.constructor === Array;
+  return !!a && a.constructor === Array;
 }
 
 function isObject(o): boolean {
-	return !!o && o.constructor === Object;
+  return !!o && o.constructor === Object;
 }
 
 function chunkArray<T>(a: T[], limit = 0) {
-	if (!isArray(a)) throw new Error('Provide an array');
-	const _a: Array<Array<T>> = [],
-		chunks = Math.ceil(a.length / limit);
+  if (!isArray(a)) throw new Error('Provide an array');
+  const _a: Array<Array<T>> = [],
+    chunks = Math.ceil(a.length / limit);
 
-	if (chunks < 2 || limit === 0) return [a];
+  if (chunks < 2 || limit === 0) return [a];
 
-	for (let i = 0; i < chunks; i++) {
-		_a.push(a.slice(i * limit, i * limit + limit));
-	}
-	return _a;
+  for (let i = 0; i < chunks; i++) {
+    _a.push(a.slice(i * limit, i * limit + limit));
+  }
+  return _a;
 }
 
 export { isArray, isObject, chunkArray };
